a05c1a1a34e8f7c3b07b3da4fa92d5fef48bc962
Krystine-825 <119885432+Krystine-825@users.noreply.github.com>
2023-12-11 23:29:54 +0700

    Update SelectionSort.java

--- Strategy/SelectionSort.java
+++ Strategy/SelectionSort.java
@@ -3,7 +3,7 @@ import java.util.List;
 public class SelectionSort implements Strategy {
     @Override
     public void sort(List<Integer> list) {
-        System.out.print("Sort by Selection Sort: ");
+        System.out.print("Selection Sort: ");
         for (int i = 0; i < list.size(); i++) {
             int min = i;
             for (int j = i + 1; j < list.size(); j++)

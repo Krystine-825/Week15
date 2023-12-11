8fd01da58d6f4f189080bdf9f623745e151c6506
Krystine-825 <119885432+Krystine-825@users.noreply.github.com>
2023-12-11 23:29:22 +0700

    Update BubbleSort.java

--- Strategy/BubbleSort.java
+++ Strategy/BubbleSort.java
@@ -3,7 +3,7 @@ import java.util.List;
 public class BubbleSort implements Strategy {
     @Override
     public void sort(List<Integer> list) {
-        System.out.print("Sort by Bubble Sort: ");
+        System.out.print("Bubble Sort: ");
         for (int i = 0; i < list.size() - 1; i++) {
             boolean swapped = false;
             for (int j = 0; j < list.size() - i - 1; j++) {
